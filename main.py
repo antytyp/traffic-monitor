46f3edd5e20705f576ebc9a2d2438834f0101c7c
antytyp <pwdyrlaga@gmail.com>
2024-05-27 21:59:14 +0200

    introduce constants file

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 from dotenv import load_dotenv
 
+from src import constants
 from src.algorithms.statistical_traffic_monitor_model import (
     StatisticalTrafficMonitorModel,
 )
@@ -15,15 +16,12 @@ from src.data_setup.traffic_video_frame_preprocessor import (
 from src.data_setup.traffic_video_stream import TrafficVideoStream
 from src.utils.utils import get_monitored_regions
 
-DEFAULT_CONFIG_PATH = "config/traffic_monitor_config.json"
-NUM_ITERATIONS = 10
-
 
 if __name__ == "__main__":
     # Load environment variables from .env file
     load_dotenv()
 
-    with open(DEFAULT_CONFIG_PATH) as f:
+    with open(constants.DEFAULT_CONFIG_PATH) as f:
         config = json.load(f)
 
     camera_stream_url = os.getenv("CAMERA_STREAM_URL")
@@ -36,7 +34,7 @@ if __name__ == "__main__":
     traffic_monitor_model = StatisticalTrafficMonitorModel(monitored_regions)
     frame_postprocessor = TrafficVideoFramePostprocessor(monitored_regions)
 
-    for _ in range(NUM_ITERATIONS):
+    for _ in range(constants.NUM_ITERATIONS):
         frame = traffic_video_stream.get_frame()
         prepared_frame = frame_preprocessor.prepare(frame)
         predictions = traffic_monitor_model.predict(prepared_frame)
---
+++ src/constants.py
@@ -0,0 +1,2 @@
+DEFAULT_CONFIG_PATH = "config/traffic_monitor_config.json"
+NUM_ITERATIONS = 10
