337e6645b0c1157a0096a399b93ac9d522753dff
antytyp <pwdyrlaga@gmail.com>
2024-05-28 12:33:35 +0200

    introduce open and release connection functions

--- src/data_setup/traffic_video_stream.py
+++ src/data_setup/traffic_video_stream.py
@@ -7,16 +7,22 @@ import cv2
 class TrafficVideoStream:
     def __init__(self, stream_url: Union[str, None]):
         self.stream_url = stream_url
+        self.cv2_video_capture = cv2.VideoCapture()
 
-    def get_frame(self) -> cv2.typing.MatLike:
-        cap = cv2.VideoCapture()
-        cap.open(self.stream_url)
+    def open_connection(self) -> None:
+        self.cv2_video_capture.open(self.stream_url)
         print(
             f'VideoCapture connection opened at {datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")}.'
         )
-        ret, frame = cap.read()
-        cap.release()
+
+    def release_connection(self) -> None:
+        self.cv2_video_capture.release()
         print(
             f'VideoCapture connection released at {datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")}.'
         )
+
+    def get_frame(self) -> cv2.typing.MatLike:
+        self.open_connection()
+        ret, frame = self.cv2_video_capture.read()
+        self.release_connection()
         return frame
