aca823b3bc8d837d38f12873677ed80f1be6fbbe
antytyp <pwdyrlaga@gmail.com>
2024-06-04 20:56:43 +0200

    add _get_m3u8_url method

--- src/data_setup/data_downloader.py
+++ src/data_setup/data_downloader.py
@@ -1,5 +1,7 @@
 import logging
 
+import requests  # type: ignore
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -8,8 +10,19 @@ class DataDownloader:
     def __init__(self, stream_url: str) -> None:
         self.stream_url = stream_url
         self.base_url = self._get_base_url()
+        self.m3u8_url = self._get_m3u8_url()
 
     def _get_base_url(self) -> str:
         base_url = self.stream_url.rstrip(self.stream_url.split("/")[-1])
         logger.info(f"Prepared base url: {base_url}")
         return base_url
+
+    def _get_m3u8_url(self) -> str:
+        response = requests.get(self.stream_url)
+        response.raise_for_status()
+        m3u8_url_suffix = next(
+            (key for key in response.text.split("\n") if key.endswith(".m3u8")),
+        )
+        m3u8_url = self.base_url + m3u8_url_suffix
+        logger.info(f"Prepared m3u8 url: {m3u8_url}")
+        return m3u8_url
